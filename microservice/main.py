505a6e21dd96fe2bc40ee6029e69b2b4875a72bc
Piotr Hondra <piotr.hondra@gmail.com>
2022-01-14 19:49:54 +0100

    added endpoint for simple model

--- microservice/main.py
+++ microservice/main.py
@@ -11,11 +11,23 @@ from RFM_model.features.build_features import FeatureBuilder
 from RFM_model.models.train_model import Trainer
 from RFM_model.models.predict_model import Predictor
 import os
+
+from IUM21Z_Zad_05_03.features.build_features import FeaturesSimpleModel
+from IUM21Z_Zad_05_03.models.predict_model import SimpleModelPredictor
+from IUM21Z_Zad_05_03.models.train_model import SimpleModelTrainer
+
+
 print(os.getcwd())
 
 FeatureBuilder.from_files('../data/raw').build_save("../data/processed")
 Trainer(3, 42, "../data/processed/processed.csv").train_save("../models")
-pr = Predictor("../models")
+predictor = Predictor("../models")
+
+FeaturesSimpleModel.from_files(
+    "../data/raw").generate_processed_files("../data/processed")
+SimpleModelTrainer(
+    "../data/processed/train/processed.csv").train_save_simple_model("../models")
+simple_predictor = SimpleModelPredictor("../models")
 
 app = FastAPI()
 
@@ -27,14 +39,24 @@ class DataTables(BaseModel):
 
 
 @app.post('/predict')
-async def predict_species(tables: DataTables):
+async def predict(tables: DataTables):
     data = tables.dict()
     x = FeatureBuilder.from_json(
         sessions=data["sessions"], users=data["users"], products=data["products"]).build()
-    print(x)
 
-    f = pr.predict(x)
+    f = predictor.predict(x)
 
-    f.to_json(orient="records")
+    return f.to_json(orient="records")
 
+
+@app.post('/predict_simple')
+async def predict_simple(tables: DataTables):
+    data = tables.dict()
+    sess = pd.read_json(data["sessions"])
+
+    x = FeaturesSimpleModel.from_json(
+        sessions=data["sessions"], users=data["users"], products=data["products"]).merge_dataframes_and_add_attributes(sess)
+    result_frame = simple_predictor.predict_simple_model(x)
+    best_group = simple_predictor.best_group(result_frame)
+    f = result_frame[result_frame.Cluster == best_group].user_id
     return f.to_json(orient="records")
